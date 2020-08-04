d64e72d53b70b3edfa0ebee01bca237f8aab6e00
kim96-gitb <eoe25123@naver.com>
2020-08-04 22:58:23 +0900

    언팔

--- controller/follow.js
+++ controller/follow.js
@@ -20,8 +20,41 @@ exports.follow = async function (req, res, next) {
   } catch (e) {
     if (e.errno == 1062) {
       res.status(500).json({ success: false, msg: "이미 팔로우를 하셨습니다" });
+      return;
     } else {
       res.status(400).json({ success: false, msg: e });
     }
+  }
+};
+// @desc 언팔
+// @routes DELETE api/v1/follow/un
+// @request token , following_id
+// @response success
+exports.unfollow = async (req, res, next) => {
+  let user_id = req.user.user_id;
+  let following_id = req.body.following_id;
+
+  let query = `select * from sns_follow where user_id = ${user_id} and following_id = ${following_id}`;
+  try {
+    [rows] = await connection.query(query);
+    let saved_following_id = rows[0].following_id;
+    if (saved_following_id == following_id) {
+      res
+        .status(500)
+        .json({
+          success: false,
+          msg: "이미 삭제되거나 존재 하지않는 유저입니다",
+        });
+    }
+  } catch (e) {
+    res.status(400).json({ success: false, msg: e });
+  }
+
+  query = `delete from sns_follow where user_id = ${user_id} and following_id = ${following_id}`;
+  try {
+    [result] = await connection.query(query);
+    res.status(200).json({ success: true, msg: result });
+  } catch (e) {
+    res.status(400).json({ success: false, msg: e });
   }
 };
--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const { follow } = require("../controller/follow");
+const { follow, unfollow } = require("../controller/follow");
 
 const router = express.Router();
 
 router.route("/").post(auth, follow);
+router.route("/un").delete(auth, unfollow);
 
 module.exports = router;
