9ceef0524c0ed9f51c0b3986a5f058ed0dc5de78
kim96-gitb <67768245+kim96-gitb@users.noreply.github.com>
2020-08-06 17:10:45 +0900

    dd

--- routes/posting.js
+++ routes/posting.js
@@ -4,13 +4,13 @@ const {
   update_photo,
   delete_photo,
   followPosting,
-  uploadPhoto,
+  photoPosting,
 } = require("../controller/posting");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/").post(auth, uploadPhoto);
+router.route("/").post(auth, photoPosting);
 router.route("/me").get(auth, myposting);
 router.route("/update").put(auth, update_photo);
 router.route("/delete").delete(auth, delete_photo);
